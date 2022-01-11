b6adb86f40180eab8f73aa8345a3c2ab3db8d68f
Bob bobb <thnovtntjmmbdquipw@niwghx.online>
2022-01-11 17:07:14 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.File = new System.Windows.Forms.ToolStripDropDownButton();
+            this.New = new System.Windows.Forms.ToolStripMenuItem();
+            this.Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save_as = new System.Windows.Forms.ToolStripMenuItem();
+            this.CloseBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.Edit = new System.Windows.Forms.ToolStripDropDownButton();
+            this.Copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.Paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.Cut = new System.Windows.Forms.ToolStripMenuItem();
+            this.Delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkThemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.Edit,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // File
+            // 
+            this.File.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.New,
+            this.Open,
+            this.Save,
+            this.Save_as,
+            this.CloseBtn});
+            this.File.Image = ((System.Drawing.Image)(resources.GetObject("File.Image")));
+            this.File.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(38, 22);
+            this.File.Text = "File";
+            // 
+            // New
+            // 
+            this.New.Name = "New";
+            this.New.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.New.Size = new System.Drawing.Size(193, 22);
+            this.New.Text = "New";
+            this.New.Click += new System.EventHandler(this.New_Click);
+            // 
+            // Open
+            // 
+            this.Open.Name = "Open";
+            this.Open.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.Open.Size = new System.Drawing.Size(193, 22);
+            this.Open.Text = "Open";
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.Save.Size = new System.Drawing.Size(193, 22);
+            this.Save.Text = "Save";
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Save_as
+            // 
+            this.Save_as.Name = "Save_as";
+            this.Save_as.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.Save_as.Size = new System.Drawing.Size(193, 22);
+            this.Save_as.Text = "Save as...";
+            this.Save_as.Click += new System.EventHandler(this.Save_as_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.ShortcutKeyDisplayString = "Alt + F4";
+            this.CloseBtn.Size = new System.Drawing.Size(193, 22);
+            this.CloseBtn.Text = "Close";
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // Edit
+            // 
+            this.Edit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Edit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Copy,
+            this.Paste,
+            this.Cut,
+            this.Delete});
+            this.Edit.Image = ((System.Drawing.Image)(resources.GetObject("Edit.Image")));
+            this.Edit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(40, 22);
+            this.Edit.Text = "Edit";
+            // 
+            // Copy
+            // 
+            this.Copy.Name = "Copy";
+            this.Copy.ShortcutKeyDisplayString = "Ctrl + C";
+            this.Copy.Size = new System.Drawing.Size(150, 22);
+            this.Copy.Text = "Copy";
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Name = "Paste";
+            this.Paste.ShortcutKeyDisplayString = "Ctrl + V";
+            this.Paste.Size = new System.Drawing.Size(150, 22);
+            this.Paste.Text = "Paste";
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Cut
+            // 
+            this.Cut.Name = "Cut";
+            this.Cut.ShortcutKeyDisplayString = "Ctrl + X";
+            this.Cut.Size = new System.Drawing.Size(150, 22);
+            this.Cut.Text = "Cut";
+            this.Cut.Click += new System.EventHandler(this.Cut_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Name = "Delete";
+            this.Delete.ShortcutKeyDisplayString = "Del";
+            this.Delete.Size = new System.Drawing.Size(150, 22);
+            this.Delete.Text = "Delete";
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(785, 338);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.darkThemeToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(58, 22);
+            this.toolStripDropDownButton1.Text = "Format";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // darkThemeToolStripMenuItem
+            // 
+            this.darkThemeToolStripMenuItem.Name = "darkThemeToolStripMenuItem";
+            this.darkThemeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.darkThemeToolStripMenuItem.Text = "Dark Theme";
+            this.darkThemeToolStripMenuItem.Click += new System.EventHandler(this.darkThemeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 362);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "Form1";
+            this.Text = "Notepadzik";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton File;
+        private System.Windows.Forms.ToolStripMenuItem New;
+        private System.Windows.Forms.ToolStripMenuItem Open;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.ToolStripMenuItem Save_as;
+        private System.Windows.Forms.ToolStripMenuItem CloseBtn;
+        private System.Windows.Forms.ToolStripDropDownButton Edit;
+        private System.Windows.Forms.ToolStripMenuItem Copy;
+        private System.Windows.Forms.ToolStripMenuItem Paste;
+        private System.Windows.Forms.ToolStripMenuItem Cut;
+        private System.Windows.Forms.ToolStripMenuItem Delete;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkThemeToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private string CurrentFileDir = null;
+        private bool TextUnsaved = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ChangeTitle(TextUnsaved);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            if (UnsavedWarning())
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private bool UnsavedWarning()
+        {
+            if (TextUnsaved)
+            {
+                if (MessageBox.Show("Are you sure? Unsaved data will be lost!", "Warning", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void ChangeTitle(bool isUnsaved)
+        {
+            if (CurrentFileDir != null)
+            {
+                this.Text = CurrentFileDir + " - Notepadzik";
+            }
+            else
+            {
+                this.Text = "No title - Notepadzik";
+            }
+
+            if (isUnsaved)
+            {
+                this.Text += " !UNSAVED!";
+            }
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            if (UnsavedWarning())
+                return;
+
+            CurrentFileDir = null;
+            richTextBox1.Text = "";
+            TextUnsaved = false;
+            ChangeTitle(TextUnsaved);
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            if (UnsavedWarning())
+                return;
+
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Open";
+            openFile.InitialDirectory = Application.StartupPath;
+            openFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFile.Multiselect = false;
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                CurrentFileDir = openFile.FileName;
+                richTextBox1.LoadFile(CurrentFileDir, RichTextBoxStreamType.PlainText);
+                TextUnsaved = false;
+                ChangeTitle(TextUnsaved);
+            }
+        }
+
+        private void SaveAsWindow()
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "Save as";
+            saveFile.InitialDirectory = Application.StartupPath;
+            saveFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                CurrentFileDir = saveFile.FileName;
+                richTextBox1.SaveFile(CurrentFileDir, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void Save_as_Click(object sender, EventArgs e)
+        {
+            SaveAsWindow();
+            TextUnsaved = false;
+            ChangeTitle(TextUnsaved);
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (CurrentFileDir != null)
+            {
+                richTextBox1.SaveFile(CurrentFileDir, RichTextBoxStreamType.PlainText);
+            }
+            else
+            {
+                SaveAsWindow();
+            }
+
+            TextUnsaved = false;
+            ChangeTitle(TextUnsaved);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextUnsaved = true;
+            ChangeTitle(TextUnsaved);
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void Cut_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = "";
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontwindow = new FontDialog();
+            fontwindow.Font = richTextBox1.Font;
+            
+            if (fontwindow.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.Font = fontwindow.Font;
+            }
+        }
+
+        private void darkThemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            darkThemeToolStripMenuItem.Checked = !darkThemeToolStripMenuItem.Checked;
+
+            if(darkThemeToolStripMenuItem.Checked)
+            {
+                richTextBox1.BackColor = Color.FromArgb(255,46, 46, 46);
+                richTextBox1.ForeColor = Color.FromArgb(255, 199, 199, 199);
+            }
+            else
+            {
+                richTextBox1.BackColor = Color.White;
+                richTextBox1.ForeColor = Color.Black;
+            }
+
+            TextUnsaved = false;
+            ChangeTitle(TextUnsaved);
+        }
+    }
+}
